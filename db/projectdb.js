6529d5dd9ca17d42150a0101e27a8c2b2172670c
Deeksha Sharma <Deeksha Sharma>
2020-04-11 02:31:51 +0530

    updated

--- db/projectdb.js
+++ db/projectdb.js
@@ -1,7 +1,8 @@
  var mongoose = require("mongoose");
  require('dotenv').config();
 
- var conn = mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@database-5ivou.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true  , useUnifiedTopology: true} , (err)=>{
+ var url = process.env.MONGO_URI;
+ var conn = mongoose.connect(process.env.MONGODB_URI || url , { useNewUrlParser: true  , useUnifiedTopology: true} , (err)=>{
      if(!err){
          console.log("Moongoose connect succeded...");
      }
